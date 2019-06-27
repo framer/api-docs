e6f56af4fb3f1f143b25f5014e3f4b706dc1eca9
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2019-06-27 12:20:54 +0200

    Improve EmbeddedExample layout

--- components/layout/EmbeddedExample.tsx
+++ components/layout/EmbeddedExample.tsx
@@ -1,26 +1,33 @@
+import * as React from "react"
 import styled from "styled-components"
 import { desktop } from "./Breakpoints"
 
-export const EmbeddedExample = styled.div<{ height?: number; background?: string }>`
+const EmbeddedExampleBackground = styled.div<{ height?: number; background?: string }>`
     display: flex;
     place-content: center;
     place-items: center;
     height: ${props => props.height || "320px"};
     background: ${props => props.background || "#151515"};
     border-radius: 8px;
-    margin: 40px 0;
+    margin: 20px 0;
     width: 100%;
     max-width: 100%;
     overflow: hidden;
     user-select: none;
 
     @media (min-width: ${desktop}) {
-        &:first-child {
-            margin-top: 0;
-        }
-
-        &:last-child {
-            margin-bottom: 0;
-        }
+        margin: 0;
     }
 `
+
+interface EmbeddedExampleProps {
+    children: any
+    height?: number
+    background?: string
+}
+
+export const EmbeddedExample = ({ children, height, background }: EmbeddedExampleProps) => (
+    <EmbeddedExampleBackground height={height} background={background}>
+        {children}
+    </EmbeddedExampleBackground>
+)
