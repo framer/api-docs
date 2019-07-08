8ebcfdb017d64842389bcac3b32d46999e0c4002
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2019-07-08 17:09:23 +0200

    Flatten MotionValueSVG example

--- components/examples/motion/MotionValueSVG.tsx
+++ components/examples/motion/MotionValueSVG.tsx
@@ -31,18 +31,13 @@ const ProgressIcon = styled.svg`
 function StaticMotionValueSVG() {
     const x = useMotionValue(0)
     const xInput = [-100, 0, 100]
-    const background = useTransform(x, xInput, [
-        "linear-gradient(180deg, #ff008c 0%, rgb(211, 9, 225) 100%)",
-        "linear-gradient(180deg, #7700ff 0%, rgb(68, 0, 255) 100%)",
-        "linear-gradient(180deg, rgb(230, 255, 0) 0%, rgb(3, 209, 0) 100%)",
-    ])
-    const color = useTransform(x, xInput, ["rgb(211, 9, 225)", "rgb(68, 0, 255)", "rgb(3, 209, 0)"])
+    const color = useTransform(x, xInput, ["#f08", "#05f", "#2dd"])
     const tickPath = useTransform(x, [10, 100], [0, 1])
     const crossPathA = useTransform(x, [-10, -55], [0, 1])
     const crossPathB = useTransform(x, [-50, -100], [0, 1])
 
     return (
-        <Container style={{ background }}>
+        <Container style={{ background: color }}>
             <Box style={{ x }} drag="x" dragConstraints={{ left: 0, right: 0 }}>
                 <ProgressIcon viewBox="0 0 50 50">
                     <motion.path
