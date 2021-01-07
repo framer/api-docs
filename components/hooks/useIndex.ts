0c452e79d48a222479be3228deb0c6d991518a44
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-01-07 12:28:18 +0100

    Fix and simplify useIndexItem

--- components/hooks/useIndex.ts
+++ components/hooks/useIndex.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect, Dispatch, SetStateAction } from "react"
+import { useCallback, useState, useEffect, useRef, Dispatch, SetStateAction } from "react"
 
 const clamp = (value: number, min: number, max: number) => {
     return Math.min(Math.max(value, min), max)
@@ -12,38 +12,26 @@ const wrap = (value: number, min: number, max: number) => {
 
 export type IndexOperator = (nudge?: number) => void
 
-export const useIndex = (
-    range: number,
-    initial = 0
-): [number, IndexOperator, IndexOperator, Dispatch<SetStateAction<number>>] => {
-    const [index, setIndex] = useState(initial)
-
-    useEffect(() => {
-        setIndex(index => clamp(index, 0, Math.max(range - 1, 0)))
-    }, [range])
-
-    const previousIndex = useCallback(
-        (nudge = 1) => {
-            setIndex(index => wrap(index - nudge, 0, Math.max(range, 0)))
-        },
-        [range]
-    )
-
-    const nextIndex = useCallback(
-        (nudge = 1) => {
-            setIndex(index => wrap(index + nudge, 0, Math.max(range, 0)))
-        },
-        [range]
-    )
-
-    return [index, previousIndex, nextIndex, setIndex]
-}
-
 export const useIndexItem = <T>(
     items: T[],
     initial = 0
 ): [T, number, IndexOperator, IndexOperator, Dispatch<SetStateAction<number>>] => {
-    const [index, ...indexOperators] = useIndex(items.length, initial)
+    const [index, setIndex] = useState(initial)
+    const itemsRef = useRef(items)
 
-    return [items[index], index, ...indexOperators]
+    useEffect(() => {
+        itemsRef.current = items
+
+        setIndex(index => clamp(index, 0, Math.max(items.length - 1, 0)))
+    }, [items])
+
+    const previousItem = useCallback((nudge: number = 1) => {
+        setIndex(index => wrap(index - nudge, 0, Math.max(itemsRef.current.length, 0)))
+    }, [])
+
+    const nextItem = useCallback((nudge: number = 1) => {
+        setIndex(index => wrap(index + nudge, 0, Math.max(itemsRef.current.length, 0)))
+    }, [])
+
+    return [items[index], index, previousItem, nextItem, setIndex]
 }
